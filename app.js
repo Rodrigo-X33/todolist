52ebb168f9dbf3145966ae0ff0fb236e44928b54
Rodrigo-X33 <rodrigo.chaconx33@gmail.com>
2024-01-22 08:46:20 -0600

    Prueba de remplazo de los modulos con el .gitignore

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { render } = require('ejs');
-const { getDate } = require('./node_modules/getDay');
 
 const app = express();
 
